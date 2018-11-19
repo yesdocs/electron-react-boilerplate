b31c2b4278b175674cee89b9b2e6cdc44a1ebdd6
George Mckenzie <george@yesdocs.com>
2018-11-19 11:58:10 -0800

    sample calc application

--- app/Routes.js
+++ app/Routes.js
@@ -4,12 +4,12 @@ import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import Calculator from './containers/Calculator';
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
+      <Route path={routes.CALC} component={Calculator} />
     </Switch>
   </App>
 );
---
+++ app/components/CalculatorDisplay.js
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+import ScalingText from './ScalingText';
+import styles from './CalculatorDisplay.css';
+import routes from '../constants/routes';
+
+export default class CalculatorDisplay extends React.Component {
+    render() {
+      const { value, ...props } = this.props
+      
+      const language = navigator.language || 'en-US'
+      let formattedValue = parseFloat(value).toLocaleString(language, {
+        useGrouping: true,
+        maximumFractionDigits: 6
+      })
+      
+      // Add back missing .0 in e.g. 12.0
+      const match = value.match(/\.\d*?(0*)$/)
+      
+      if (match)
+        formattedValue += (/[1-9]/).test(match[0]) ? match[1] : match[0]
+      
+      return (
+        <div {...props} className={styles.calculatordisplay}>
+          <ScalingText>{formattedValue}</ScalingText>
+        </div>
+      )
+    }
+  }
+  
